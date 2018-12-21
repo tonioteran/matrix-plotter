95da8e0b2d97d7b61f515bfa90af241432d22c64
antonio teran <teran@mit.edu>
2018-12-21 01:17:52 +0100

    initial library commit!

---
+++ tests/testMatrixPlotter.cpp
@@ -0,0 +1,16 @@
+/**
+ * @file testMatrixPlotter.cpp
+ * @brief tester for MatrixPlotter class
+ * @date December 21, 2018
+ * @author Antonio Teran, teran@mit.edu
+ */
+
+#include <iostream>
+
+int main(void) {
+
+  std::cout << "hello MatrixPlotter world!!!" << std::endl;
+
+  return (0);
+
+}
